664ae2ec498e32933f7511671513549a5969e56f
ehxa <2042918@student.uma.pt>
2024-04-28 17:04:48 +0100

    Added Formatter

--- src/CSVToARFFConverter.java
+++ src/CSVToARFFConverter.java
@@ -2,12 +2,16 @@ import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
 import java.io.*;
+import java.util.Scanner;
 
 public class CSVToARFFConverter {
     public static void convertCSVToARFF() throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Insert the name of the csv: ");
+        String fileName = input.nextLine();
         CSVLoader loader = new CSVLoader();
-        String sourceFilePath = "../Output/csv/Japan.csv";
-        String destinationFilePath = "../Output/arff/Japan.arff";
+        String sourceFilePath = "../Output/csv/" + fileName + ".csv";
+        String destinationFilePath = "../Output/arff/" + fileName + ".arff";
         loader.setSource(new File(sourceFilePath));
         Instances data = loader.getDataSet();
         System.out.println(data.get(0));
